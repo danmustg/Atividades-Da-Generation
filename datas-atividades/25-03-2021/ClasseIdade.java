19f31122b8f847190b93085d3bb4969ed69b9dc5
Danilo Mustang <80558886+danmustg@users.noreply.github.com>
2021-03-25 14:25:29 -0300

    Add files via upload

---
+++ datas-atividades/25-03-2021/ClasseIdade.java
@@ -0,0 +1,33 @@
+package Turma20;
+
+import java.util.Scanner;
+public class ClasseIdade {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int idade;
+        
+		   Scanner ler= new Scanner(System.in);
+		
+		System.out.printf("Qual sua idade? ");
+		idade= ler.nextInt();
+		
+		 
+		  System.out.printf("Classe: ");
+		
+		if (idade>=10 && idade<=14)
+		{
+		 System.out.println("Infantil ");
+		}
+		else if (idade>=15 && idade<=17)
+		{
+		 System.out.println("Juvenil ");
+		}
+		else if (idade>=18 && idade<=25)
+		{
+			System.out.println("Adulto ");
+		}
+     
+	}
+
+}
---
+++ datas-atividades/25-03-2021/MaiorR.java
@@ -0,0 +1,36 @@
+package Turma20;
+
+import java.util.Scanner;
+public class MaiorR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner imput= new Scanner(System.in);
+		int n1,n2,n3,soma,max,min,nu3=0;
+		
+		System.out.print("Digite Um Numero: ");
+		n1= imput.nextInt();
+		
+		System.out.print("Digite Segundo Numero: ");
+		n2= imput.nextInt();
+		
+		System.out.print("Digite Terceiro Numero: ");
+		n3= imput.nextInt();
+		
+		
+		if (n1>n2 && n1>n3)
+		{
+			System.out.println("O maior valor é "+n1);
+		}
+		else if (n2>n1 && n2>n3)
+		{
+			System.out.println("O maior valor é "+n2);
+		}
+		else if (n3>n1 && n3>n2)
+		{
+			System.out.println("O maior valor é :"+n3);
+		}
+	}
+ 
+}
